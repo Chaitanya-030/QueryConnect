ee5ce252759d2fd43df6b6a0eefd9e3dcd787b74
chaitanya <csheth845@gmail.com>
2024-06-25 10:26:26 +0530

    objectName as variable added

--- demo/src/main/java/com/example/demo/App.java
+++ demo/src/main/java/com/example/demo/App.java
@@ -34,16 +34,19 @@ public class App {
     	mp.put("artist", "http://dbpedia.org/ontology/artist");
     	mp.put("Leonardo_DiCaprio", "http://dbpedia.org/resource/Leonardo_DiCaprio"); //
     	mp.put("Film", "http://dbpedia.org/ontology/Film"); //
-    	mp.put("Album", "http://dbpedia.org/ontology/Album");
+    	mp.put("Album", "http://dbpedia.org/ontology/Album"); //
     	mp.put("Microsoft", "http://dbpedia.org/resource/Microsoft"); //
-        mp.put("The_Beatles", "http://dbpedia.org/resource/The_Beatles");
-        mp.put("Charles_Dickens", "http://dbpedia.org/resource/Charles_Dickens");
-        mp.put("VideoGame", "http://dbpedia.org/ontology/VideoGame");
-        mp.put("Book", "http://dbpedia.org/ontology/Book");
-        mp.put("author", "http://dbpedia.org/ontology/author");
-        mp.put("country", "http://dbpedia.org/ontology/country");
-        mp.put("River", "http://dbpedia.org/ontology/River");
-        mp.put("Nintendo", "http://dbpedia.org/resource/Nintendo");
+        mp.put("The_Beatles", "http://dbpedia.org/resource/The_Beatles"); //
+        mp.put("Charles_Dickens", "http://dbpedia.org/resource/Charles_Dickens"); //
+        mp.put("VideoGame", "http://dbpedia.org/ontology/VideoGame"); //
+        mp.put("Book", "http://dbpedia.org/ontology/Book"); //
+        mp.put("author", "http://dbpedia.org/ontology/author"); //
+        mp.put("country", "http://dbpedia.org/ontology/country"); //
+        mp.put("River", "http://dbpedia.org/ontology/River"); //
+        mp.put("nationality", "http://dbpedia.org/ontology/nationality"); //
+        mp.put("currency", "http://dbpedia.org/ontology/currency");
+        mp.put("Nintendo", "http://dbpedia.org/resource/Nintendo"); //
+        mp.put("Japan", "http://dbpedia.org/resource/Japan"); 
     	
 
         // Convert the KQL string into the SPARQL compatible string
@@ -93,18 +96,21 @@ public class App {
         kqlQuery = kqlQuery.replace(" India ", " kql:India "); // 
         kqlQuery = kqlQuery.replace(" starring ", " kql:starring "); //
         kqlQuery = kqlQuery.replace(" Leonardo_DiCaprio ", " kql:Leonardo_DiCaprio "); //
-        kqlQuery = kqlQuery.replace(" artist ", " kql:artist ");
-        kqlQuery = kqlQuery.replace(" Album ", " kql:Album ");
+        kqlQuery = kqlQuery.replace(" artist ", " kql:artist "); //
+        kqlQuery = kqlQuery.replace(" Album ", " kql:Album "); //
         kqlQuery = kqlQuery.replace(" Film ", " kql:Film "); //
         kqlQuery = kqlQuery.replace(" Microsoft ", " kql:Microsoft "); //
-        kqlQuery = kqlQuery.replace(" The_Beatles ", " kql:The_Beatles ");
-        kqlQuery = kqlQuery.replace(" VideoGame ", " kql:VideoGame ");
-        kqlQuery = kqlQuery.replace(" Nintendo ", " kql:Nintendo ");
-        kqlQuery = kqlQuery.replace(" River ", " kql:River ");
-        kqlQuery = kqlQuery.replace(" country ", " kql:country ");
-        kqlQuery = kqlQuery.replace(" Charles_Dickens ", " kql:Charles_Dickens ");
-        kqlQuery = kqlQuery.replace(" Book ", " kql:Book ");
-        kqlQuery = kqlQuery.replace(" author ", " kql:author ");
+        kqlQuery = kqlQuery.replace(" The_Beatles ", " kql:The_Beatles "); //
+        kqlQuery = kqlQuery.replace(" VideoGame ", " kql:VideoGame "); //
+        kqlQuery = kqlQuery.replace(" Nintendo ", " kql:Nintendo "); //
+        kqlQuery = kqlQuery.replace(" River ", " kql:River "); //
+        kqlQuery = kqlQuery.replace(" country ", " kql:country "); //
+        kqlQuery = kqlQuery.replace(" Charles_Dickens ", " kql:Charles_Dickens "); //
+        kqlQuery = kqlQuery.replace(" Book ", " kql:Book "); //
+        kqlQuery = kqlQuery.replace(" author ", " kql:author "); //
+        kqlQuery = kqlQuery.replace(" nationality ", " kql:nationality ");
+        kqlQuery = kqlQuery.replace(" Japan ", " kql:Japan ");
+        kqlQuery = kqlQuery.replace(" currency ", " kql:currency ");
 
         return kqlQuery;
     }
@@ -112,14 +118,20 @@ public class App {
     public static Triple convertToDBpediaTriple(Node subjectNode, String predicateURI, String objectURI) {
 
         String predicateName = predicateURI.substring(predicateURI.lastIndexOf('#') + 1);
-        String objectName = objectURI.substring(objectURI.lastIndexOf('#') + 1);
-       
+        System.out.println(objectURI);
+    
         String dbpediapredicateURI = mp.get(predicateName);
-        String dbpediaobjectURI = mp.get(objectName);
-        
         Node predicateNode = NodeFactory.createURI(dbpediapredicateURI);
-        Node objectNode = NodeFactory.createURI(dbpediaobjectURI);
-
+    
+        Node objectNode;
+        if (objectURI.contains("?")) {
+            objectNode = NodeFactory.createVariable(objectURI.substring(1));
+        } else {
+            String objectName = objectURI.substring(objectURI.lastIndexOf('#') + 1);
+            String dbpediaobjectURI = mp.get(objectName);
+            objectNode = NodeFactory.createURI(dbpediaobjectURI);
+        }
+    
         return Triple.create(subjectNode, predicateNode, objectNode);
     }
 
@@ -136,7 +148,16 @@ public class App {
                 Node subject = tr.getSubject();
                 Node predicate = tr.getPredicate();
                 Node object = tr.getObject();
-                Triple newtr = convertToDBpediaTriple(subject, predicate.getURI(), object.getURI());
+                
+                String objectURI;
+                if (object.isURI()) {
+                    objectURI = object.getURI();
+                } else {
+                    // If the object is a variable, use its name with a "?" prefix
+                    objectURI = "?" + object.getName();
+                }
+
+                Triple newtr = convertToDBpediaTriple(subject, predicate.getURI(), objectURI);
                 newPattern.add(newtr);
             }
 
